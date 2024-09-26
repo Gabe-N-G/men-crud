4ae0e227a8527751a910402042dc2e44c8147b83
g g <gabedotgutierrez@gmail.com>
2024-09-26 18:50:55 -0400

    connected to mongo

---
+++ server.js
@@ -0,0 +1,22 @@
+// Here is where we import modules
+// We begin by loading Express
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  });
+
+
+app.get("/", async (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("In the year 3000");
+});
