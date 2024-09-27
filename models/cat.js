f4c4d93da05549abd5d0993e8dd1ab647faacd14
g g <gabedotgutierrez@gmail.com>
2024-09-27 11:39:14 -0400

    Added gender dropdown to model/new

--- models/cat.js
+++ models/cat.js
@@ -4,6 +4,7 @@ const catSchema = new mongoose.Schema({
   name: { type: String, required: true },
   age: Number,
   image: String,
+  gender: String,
 })
 
 const Cat = mongoose.model('Cat', catSchema)
--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+
+
 const app = express();
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"))
+app.use(morgan("dev"))
+
 const Cat = require("./models/cat.js")
 
 mongoose.connect(process.env.MONGODB_URI);
